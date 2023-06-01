88b73b9305370fa7f520a89d92e6328e805ac7c4
RCJK33 <rafa.11.rcj@gmail.com>
2023-05-31 21:49:00 -0700

    Class 3
    Class 3 FSDI 108 CH37

---
+++ config.py
@@ -0,0 +1,7 @@
+import pymongo
+import certifi
+
+uri = "mongodb+srv://fsdi:rooter32@cluster0.q0t28gv.mongodb.net/?retryWrites=true&w=majority"
+
+client = pymongo.MongoClient(uri, tlsCAFile = certifi.where())
+db = client.get_database("organika")
--- server.py
+++ server.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, request
 import json
+from config import db
 
 app = Flask(__name__)
 
@@ -9,13 +10,69 @@ def home():
 
 @app.get('/test')
 def test():
-    return "Hello from another page"
+    return '<a href="/about ">fdsfdsf<a/>'
 
-@app.get('/about')
+@app.get('/api/about')
 def about():
     me = {'name:': 'Rafael'}
     return json.dumps(me)
 
 
-app.run(debug=True)
+#############################
+# API endpoints
+# JSON
+#############################
 
+def fix_id(obj):
+    obj['_id'] = str(obj['_id'])
+    return obj
+
+@app.get('/api/version')
+def version():
+    version = {
+        'name': 'mouse',
+        'version': 35,
+        'build': 2948
+    }
+    return json.dumps(version)
+
+@app.get('/api/products')
+def get_products():
+    products = []
+    cursor = db.products.find({})
+    for prod in cursor:
+        products.append(fix_id(prod))
+    return json.dumps(products)
+
+@app.post('/api/products')
+def save_product():
+    data = request.get_json() # This ias the object that send by client 
+    db.products.insert_one(data)
+
+    # products.append(data)
+    print(data)
+    return json.dumps(fix_id(data))
+
+@app.get('/api/report/total')
+def total():
+    dict = {'total': 0, 'count_products':0}
+    
+    cursor = db.products.find({})
+    for prod in cursor:
+        dict['total'] += prod['price']
+        dict['count_products'] += 1
+    return json.dumps(dict)
+
+@app.get('/api/categories')
+def categories():
+    categories = set()
+
+    cursor = db.products.find({})
+    for prod in cursor:
+        categories.add(str(prod['category']).lower())
+    categories = list(categories)
+    categories.sort()
+    return json.dumps(categories)
+
+
+app.run(debug=True)
