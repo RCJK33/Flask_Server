4624e89c6c596866802da0daa62b10cc758da048
RCJK33 <rafael.chavez29@uabc.edu.mx>
2024-03-16 00:48:50 +0000

    Add port to runserver script

--- runserver.sh
+++ runserver.sh
@@ -1,3 +1,7 @@
+#!/bin/bash
+
 # Initialize the Virtual Enviormet
+cd /home/rafaelcj/Flask_Server
 source venv/bin/activate
-gunicorn -w 2 -b 192.168.1.109 server:app
+gunicorn -w 2 -b 192.168.1.109:8000 server:app &
+
